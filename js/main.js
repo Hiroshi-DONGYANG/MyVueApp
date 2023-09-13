e2acb3095a0bcea8874404b7c04c2ed9adfc4361
Hiroshi-DONGYANG <38585892+Hiroshi-DONGYANG@users.noreply.github.com>
2023-09-13 17:52:58 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
+/*html*/
+import { ref } from 'vue';
+import { defineComponent } from "vue";
 
+var likeComponent = defineComponent({
+  props: {
+    message: {
+      type: String,
+      default: 'Like'
+    }
+  },
+  data(){
+    return {
+      count: 0
+    }
+  },
+  template: '<button @click="countUp">{{ message }} {{ count }}</button>',
+  methods: {
+    countUp: function(){
+      this.count++;
+      this.$emit("increment");
+    }
+  }
+});
+
+export default {
+  components: {
+    'like-component': likeComponent
+  },
+  data() {
+    return {
+      total: 0,
+      rawHtml: '<span style="color: red">This should be red.</span>'
+    }
+  },
+  methods: {
+    incrementTotal: function(){
+      this.total++;
+    }
+  }
+};
